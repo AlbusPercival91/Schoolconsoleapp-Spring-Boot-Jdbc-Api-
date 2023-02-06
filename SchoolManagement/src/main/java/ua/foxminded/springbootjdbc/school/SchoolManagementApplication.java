01c3ddb3a180ca75892d7ce729cdd376896ee423
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-02-06 20:33:31 +0200

    initial commit

---
+++ SchoolManagement/src/main/java/ua/foxminded/springbootjdbc/school/SchoolManagementApplication.java
@@ -0,0 +1,13 @@
+package ua.foxminded.springbootjdbc.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementApplication.class, args);
+	}
+
+}
---
+++ SchoolManagement/src/test/java/ua/foxminded/springbootjdbc/school/SchoolManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.foxminded.springbootjdbc.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
