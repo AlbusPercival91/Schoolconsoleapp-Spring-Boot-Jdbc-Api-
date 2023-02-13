5376cde9f58d9e36feee312ef506731708950dea
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-02-13 22:38:34 +0200

    create testdata generation:
    - create classes CourseMaker, GroupMaker, StudentMaker
    - add JavaFaker to pom.xml

---
+++ SchoolManagement/src/main/java/ua/foxminded/springbootjdbc/school/testdata/StudentMaker.java
@@ -0,0 +1,35 @@
+package ua.foxminded.springbootjdbc.school.testdata;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import com.github.javafaker.Faker;
+
+public class StudentMaker {
+    Faker faker = new Faker();
+
+    public List<String> generateNames(int quantity) {
+        return IntStream.range(0, quantity).<String>mapToObj(i -> faker.name().firstName())
+                .collect(Collectors.toCollection(() -> new ArrayList<>(quantity)));
+    }
+
+    public List<String> generateSurnames(int quantity) {
+        return IntStream.range(0, quantity).<String>mapToObj(i -> faker.name().lastName())
+                .collect(Collectors.toCollection(() -> new ArrayList<>(quantity)));
+    }
+
+    public Set<String> generateStudents(List<String> names, List<String> surnames) {
+        Set<String> list = new HashSet<>();
+        while (list.size() != 200) {
+            Arrays.asList(names, surnames).forEach(Collections::shuffle);
+            Collections.addAll(list, names.get(0) + " " + surnames.get(0));
+        }
+        return list;
+    }
+
+}
