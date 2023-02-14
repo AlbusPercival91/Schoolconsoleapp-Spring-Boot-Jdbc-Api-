d88b29003d62dcd3d485303bb661c4c72b66bf03
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-02-14 20:51:52 +0200

    create database from main
    - create file initialScript.sql to run creation a db

--- SchoolManagement/src/main/java/ua/foxminded/springbootjdbc/school/SchoolManagementApplication.java
+++ SchoolManagement/src/main/java/ua/foxminded/springbootjdbc/school/SchoolManagementApplication.java
@@ -1,13 +1,43 @@
 package ua.foxminded.springbootjdbc.school;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SchoolManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchoolManagementApplication.class, args);
-	}
+  public static void main(String[] args) throws InterruptedException {
+    File initialScriptTempFile;
+
+    try {
+      InputStream is = ClassLoader.getSystemClassLoader().getResourceAsStream("initialScript.sql");
+
+      initialScriptTempFile = File.createTempFile(String.valueOf(is.hashCode()), ".tmp");
+      initialScriptTempFile.deleteOnExit();
+
+      try (FileOutputStream out = new FileOutputStream(initialScriptTempFile)) {
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        while ((bytesRead = is.read(buffer)) != -1) {
+          out.write(buffer, 0, bytesRead);
+        }
+      }
+      String cmdQuery = "psql -U postgres -h localhost -p 5432 " + "-f " + Paths.get(initialScriptTempFile.toURI());
+      String[] envVars = { "PGPASSWORD=1234" };
+      Process runInitScript = Runtime.getRuntime().exec(cmdQuery, envVars);
+      runInitScript.waitFor();
+    } catch (IOException e) {
+      throw new IllegalArgumentException("File not found");
+    }
+
+    SpringApplication.run(SchoolManagementApplication.class, args);
+  }
 
 }
