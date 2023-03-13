2145b5e3f2a326ba4e5f72a577acc5b1b40c3891
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-03-13 19:38:53 +0200

    edit main class of SchoolManagementApplication
    - fix bug with finding initialScript.sql after maven build
    
    - refactor pom.xml

--- SchoolManagement/src/main/java/ua/foxminded/springbootjdbc/school/SchoolManagementApplication.java
+++ SchoolManagement/src/main/java/ua/foxminded/springbootjdbc/school/SchoolManagementApplication.java
@@ -1,10 +1,10 @@
 package ua.foxminded.springbootjdbc.school;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Paths;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,23 +13,25 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class SchoolManagementApplication {
 
   public static void main(String[] args) throws InterruptedException {
-    File initialScriptTempFile;
+    String initialScriptFileName = "initialScript.sql";
 
-    try {
-      InputStream is = ClassLoader.getSystemClassLoader().getResourceAsStream("initialScript.sql");
-
-      initialScriptTempFile = File.createTempFile(String.valueOf(is.hashCode()), ".tmp");
-      initialScriptTempFile.deleteOnExit();
-
-      try (FileOutputStream out = new FileOutputStream(initialScriptTempFile)) {
-        byte[] buffer = new byte[1024];
-        int bytesRead;
-
-        while ((bytesRead = is.read(buffer)) != -1) {
-          out.write(buffer, 0, bytesRead);
-        }
+    try (InputStream is = SchoolManagementApplication.class.getResourceAsStream("/" + initialScriptFileName);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+      byte[] buffer = new byte[1024];
+      int bytesRead;
+      while ((bytesRead = is.read(buffer)) != -1) {
+        baos.write(buffer, 0, bytesRead);
       }
-      String cmdQuery = "psql -U postgres -h localhost -p 5432 " + "-f " + Paths.get(initialScriptTempFile.toURI());
+
+      byte[] initialScriptBytes = baos.toByteArray();
+      File initialScriptTempFile = File.createTempFile(initialScriptFileName, ".tmp");
+      initialScriptTempFile.deleteOnExit();
+      try (FileOutputStream out = new FileOutputStream(initialScriptTempFile)) {
+        out.write(initialScriptBytes);
+      }
+
+      String cmdQuery = String.format("psql -U postgres -h localhost -p 5432 -f %s",
+          initialScriptTempFile.getAbsolutePath());
       String[] envVars = { "PGPASSWORD=1234" };
       Process runInitScript = Runtime.getRuntime().exec(cmdQuery, envVars);
       runInitScript.waitFor();
@@ -39,7 +41,6 @@ public class SchoolManagementApplication {
 
     ConfigurableApplicationContext context = SpringApplication.run(SchoolManagementApplication.class, args);
     context.close();
-
   }
 
 }
