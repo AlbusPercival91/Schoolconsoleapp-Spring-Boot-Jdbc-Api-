dc4c950a228c2dafa1d031272e00808a1ba527ba
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-03-31 19:59:30 +0200

    create new test class TestDataServiceTest
    new package ua.foxminded.springbootjdbc.school.testdata.dao add to
    src/test

---
+++ SchoolManagement/src/test/java/ua/foxminded/springbootjdbc/school/testdata/dao/TestDataServiceTest.java
@@ -0,0 +1,62 @@
+package ua.foxminded.springbootjdbc.school.testdata.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import ua.foxminded.springbootjdbc.school.facade.ConsoleMenuManager;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+class TestDataServiceTest {
+
+  @Autowired
+  private TestDataService testDataService;
+
+  @MockBean
+  private TestDataRepository repository;
+
+  @MockBean
+  private ConsoleMenuManager consoleMenuRunner;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  @DisplayName("Should randomly generated 200 students")
+  void shouldCreateStudent() {
+
+  }
+
+  @Test
+  @DisplayName("Should randomly generated 10 groups")
+  void createGroup() {
+
+  }
+
+  @Test
+  @DisplayName("Should randomly generated 10 courses")
+  void createCourse() {
+
+  }
+
+  @Test
+  @DisplayName("Should randomly assigne 200 students to 10 courses")
+  void createCourseStudentRelation() {
+
+  }
+
+  @Test
+  @DisplayName("Should return all rows in database")
+  void rowsCount() {
+
+  }
+}
