5d47a49d34ae7d75bd5c6a44dd7b69025cf61620
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-04-01 20:55:38 +0200

    complete with StudentDaoMockitoTest class

--- SchoolManagement/src/test/java/ua/foxminded/springbootjdbc/school/dao/mockito/StudentDaoMockitoTest.java
+++ SchoolManagement/src/test/java/ua/foxminded/springbootjdbc/school/dao/mockito/StudentDaoMockitoTest.java
@@ -4,11 +4,12 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
@@ -33,23 +34,25 @@ class StudentDaoMockitoTest {
   private ConsoleMenuManager consoleMenuRunner;
 
   @ParameterizedTest
-  @DisplayName("Should return true if number of students at course more than zero")
-  @CsvSource({ "Art", "Sports", "History" })
-  void testFindStudentsRelatedToCourse(String courseName) {
-    List<Student> expected = new ArrayList<>();
-    when(studentDAO.findStudentsRelatedToCourse(courseName)).thenReturn(expected);
+  @CsvSource({ "Art, 7, Jack, Sparrow", "Sports, 4, Sasha, Burdin", "History, 3, Jinny, Wesley" })
+  void shouldFindStudentsRelatedToCourse(String courseName, int groupId, String name, String surname) {
+    Student student = new Student(groupId, name, surname);
+    List<Student> students = Collections.singletonList(student);
+    when(studentDAO.findStudentsRelatedToCourse(courseName)).thenReturn(students);
     List<Student> actual = studentService.findStudentsRelatedToCourse(courseName);
 
-    Assertions.assertNotNull(actual);
-    Assertions.assertEquals(expected, actual);
-    verify(studentDAO).findStudentsRelatedToCourse(courseName);
+    Assertions.assertTrue(!actual.isEmpty());
+    Assertions.assertNotNull(student.getGroupId());
+    Assertions.assertNotNull(student.getFirstName());
+    Assertions.assertNotNull(student.getLastName());
+    Assertions.assertEquals(students, actual);
+    verify(studentDAO, Mockito.times(1)).findStudentsRelatedToCourse(courseName);
   }
 
   @ParameterizedTest
-  @DisplayName("Should return 1 if 1 student assigned to one course")
   @CsvSource({ "12, Art", "190, Literature", "19, Computer Science", "21, Geography", "193, Physical Science",
       "1, Life Science", "9, English", "2, Mathematics", "150, Sports", "7, History" })
-  void testAddStudentToTheCourse(int studentId, String courseName) {
+  void shouldAddStudentToTheCourse(int studentId, String courseName) {
     Integer expectedCount = 1;
     when(studentDAO.addStudentToTheCourse(studentId, courseName)).thenReturn(expectedCount);
     Integer actualCount = studentService.addStudentToTheCourse(studentId, courseName);
@@ -60,9 +63,8 @@ class StudentDaoMockitoTest {
   }
 
   @ParameterizedTest
-  @DisplayName("Should return true when actual and inserted student are equals")
   @CsvSource({ "1, Harry, Potter", "1, Ron, Wesley", "1, Herminone, Granger", "4, Draco, Malfoy " })
-  void testAddNewStudent(int groupId, String name, String surname) {
+  void shouldAddNewStudent(int groupId, String name, String surname) {
     Integer expectedCount = 1;
     Student student = new Student(groupId, name, surname);
     when(studentDAO.addNewStudent(any(Student.class))).thenReturn(expectedCount);
@@ -76,9 +78,8 @@ class StudentDaoMockitoTest {
   }
 
   @ParameterizedTest
-  @DisplayName("Should return 1 if student deleted from DB")
   @CsvSource({ "1", "2", "3" })
-  void testDeleteStudentByID(int studentId) {
+  void shouldDeleteStudentByID(int studentId) {
     Integer expectedCount = 1;
     when(studentDAO.deleteStudentByID(studentId)).thenReturn(expectedCount);
     Integer actualCount = studentService.deleteStudentByID(studentId);
@@ -89,10 +90,9 @@ class StudentDaoMockitoTest {
   }
 
   @ParameterizedTest
-  @DisplayName("Should return 1 if student deleted from course Table in DB")
   @CsvSource({ "12, Art", "190, Literature", "19, Computer Science", "21, Geography", "193, Physical Science",
       "1, Life Science", "9, English", "2, Mathematics", "150, Sports", "7, History" })
-  void testRemoveStudentFromCourse(int studentId, String courseName) {
+  void shouldRemoveStudentFromCourse(int studentId, String courseName) {
     Integer expectedCount = 1;
     when(studentDAO.removeStudentFromCourse(studentId, courseName)).thenReturn(expectedCount);
     Integer actualCount = studentService.removeStudentFromCourse(studentId, courseName);
@@ -103,9 +103,8 @@ class StudentDaoMockitoTest {
   }
 
   @ParameterizedTest
-  @DisplayName("Should return 1 if student updated")
   @CsvSource({ "1, 2, James, Potter", "3, 1, Fred, Wesley", "9, 1, Bartey, Krauch" })
-  void testUpdateStudentById(int updateId, int groupId, String name, String surname) {
+  void shouldUpdateStudentById(int updateId, int groupId, String name, String surname) {
     Integer expectedCount = 1;
     Student studentUpdate = new Student(groupId, name, surname);
     when(studentDAO.updateStudentById(updateId, studentUpdate)).thenReturn(expectedCount);
@@ -117,15 +116,14 @@ class StudentDaoMockitoTest {
   }
 
   @ParameterizedTest
-  @DisplayName("Should be equals expected and actual student lists")
   @CsvSource({ "1, Harry, Potter", "1, Ron, Wesley", "1, Herminone, Granger", "4, Draco, Malfoy " })
-  void testShowAllStudents(int groupId, String name, String surname) {
+  void shouldShowAllStudents(int groupId, String name, String surname) {
     List<Object> expected = new ArrayList<>();
     Student student = new Student(groupId, name, surname);
     expected.add(student);
     when(studentDAO.showAllStudents()).thenReturn(expected);
     List<Object> actual = studentService.showAllStudents();
-   
+
     Assertions.assertTrue(!expected.isEmpty() && !actual.isEmpty());
     Assertions.assertNotNull(student.getGroupId());
     Assertions.assertNotNull(student.getFirstName());
