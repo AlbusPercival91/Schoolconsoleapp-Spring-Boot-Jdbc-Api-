57517987490de0be32c6e04d9e262269d812c60c
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-04-07 05:00:05 +0200

    solved problem with invalid time-zone
    - do refactor in next commit

---
+++ SchoolManagement/src/test/java/ua/foxminded/springbootjdbc/school/config/TestDatasourceConfig.java
@@ -0,0 +1,43 @@
+package ua.foxminded.springbootjdbc.school.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class TestDatasourceConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driverClassName);
+
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public JdbcTemplate testJdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
