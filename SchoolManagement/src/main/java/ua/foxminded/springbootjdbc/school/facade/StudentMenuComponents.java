3647da9770fb52e67b48786de56b8b0f47b420ca
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-04-17 02:14:51 +0200

    add Lombok dependency to pom.xml
    - change default console print to @Slf4j using Lombok in class
    StudentMenuComponents

--- SchoolManagement/src/main/java/ua/foxminded/springbootjdbc/school/facade/StudentMenuComponents.java
+++ SchoolManagement/src/main/java/ua/foxminded/springbootjdbc/school/facade/StudentMenuComponents.java
@@ -4,14 +4,17 @@ import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
 import ua.foxminded.springbootjdbc.school.dao.StudentService;
 import ua.foxminded.springbootjdbc.school.entity.Student;
 import ua.foxminded.springbootjdbc.school.testdata.CourseMaker;
 
+@Slf4j
 @Component
 public class StudentMenuComponents {
   private final StudentService studentService;
-  
+
   @Autowired
   private CourseMaker courseMaker;
 
@@ -20,25 +23,26 @@ public class StudentMenuComponents {
   }
 
   public void findStudentsRelatedToCourseFacade(Scanner scan) {
-    System.out.println(MenuConstants.COURSE_NAME);
+    log.info(MenuConstants.COURSE_NAME);
     String courseName = scan.nextLine();
 
     if (courseMaker.generateCourses().contains(courseName)) {
       studentService.findStudentsRelatedToCourse(courseName).forEach(System.out::println);
-      System.out.println("\n" + MenuConstants.STUDENT_MENU);
+      log.info("\n" + MenuConstants.STUDENT_MENU);
     } else {
-      System.out.println(MenuConstants.WRONG_COURSE + "\n" + MenuConstants.STUDENT_MENU);
+      log.warn(MenuConstants.WRONG_COURSE);
+      log.info(MenuConstants.STUDENT_MENU);
     }
   }
 
   public void addNewStudentFacade(Scanner scan) {
-    System.out.println(MenuConstants.STUDENT_NAME);
+    log.info(MenuConstants.STUDENT_NAME);
     String firstName = scan.nextLine();
-    System.out.println(MenuConstants.STUDENT_LAST_NAME);
+    log.info(MenuConstants.STUDENT_LAST_NAME);
     String lastName = scan.nextLine();
 
     if (!firstName.isEmpty() || !lastName.isEmpty()) {
-      System.out.println(MenuConstants.GROUP_ID);
+      log.info(MenuConstants.GROUP_ID);
 
       if (scan.hasNextInt()) {
         Integer groupId = scan.nextInt();
@@ -49,98 +53,98 @@ public class StudentMenuComponents {
             groupId = null;
           }
           Student student = new Student(groupId, firstName, lastName);
-          System.out.println(studentService.addNewStudent(student) + " student added");
+          log.info(studentService.addNewStudent(student) + " student added");
         } else {
-          System.out.println(MenuConstants.GROUP_ID_NOTE);
+          log.warn(MenuConstants.GROUP_ID_NOTE);
         }
       } else {
-        System.out.println(MenuConstants.GROUP_ID_NOTE2);
+        log.warn(MenuConstants.GROUP_ID_NOTE2);
       }
     } else {
-      System.out.println(MenuConstants.EMPTY_NOTE);
+      log.warn(MenuConstants.EMPTY_NOTE);
     }
   }
 
   public void deleteStudentByIdFacade(Scanner scan) {
-    System.out.println(MenuConstants.STUDENT_ID);
+    log.info(MenuConstants.STUDENT_ID);
     int studentId = scan.nextInt();
-    System.out.println(studentService.deleteStudentByID(studentId) + " student(s) deleted");
+    log.info(studentService.deleteStudentByID(studentId) + " student(s) deleted");
   }
 
   public void addStudentToTheCourseFacade(Scanner scan) {
-    System.out.println(MenuConstants.STUDENT_ID);
+    log.info(MenuConstants.STUDENT_ID);
 
     if (scan.hasNextInt()) {
       Integer studentId = scan.nextInt();
 
       if (studentService.getStudentID().contains(studentId)) {
-        System.out.println(MenuConstants.COURSE_LIST);
+        log.info(MenuConstants.COURSE_LIST);
         courseMaker.generateCourses().forEach(System.out::println);
         String courseName = scan.next();
 
         if (courseMaker.generateCourses().contains(courseName)) {
           studentService.addStudentToTheCourse(studentId, courseName);
-          System.out.println("Student with ID: " + studentId + " assigned to the course: " + courseName);
+          log.info("Student with ID: " + studentId + " assigned to the course: " + courseName);
         } else {
-          System.out.println(MenuConstants.WRONG_COURSE);
+          log.warn(MenuConstants.WRONG_COURSE);
         }
       } else {
-        System.out.println(MenuConstants.STUDENT_ID_NOT_EXIST);
+        log.warn(MenuConstants.STUDENT_ID_NOT_EXIST);
       }
     } else {
-      System.out.println(MenuConstants.DIGITS_REQUIRED);
+      log.warn(MenuConstants.DIGITS_REQUIRED);
     }
   }
 
   public void removeStudentFromCourseFacade(Scanner scan) {
-    System.out.println(MenuConstants.STUDENT_ID);
+    log.info(MenuConstants.STUDENT_ID);
 
     if (scan.hasNextInt()) {
       Integer studentId = scan.nextInt();
 
       if (studentService.getStudentID().contains(studentId)) {
-        System.out.println(MenuConstants.COURSE_LIST);
+        log.info(MenuConstants.COURSE_LIST);
         courseMaker.generateCourses().forEach(System.out::println);
         String courseName = scan.next();
 
         if (courseMaker.generateCourses().contains(courseName)) {
-          System.out.println(studentService.removeStudentFromCourse(studentId, courseName) + " student(s) with ID "
-              + studentId + " deleted from course " + courseName);
+          log.info(studentService.removeStudentFromCourse(studentId, courseName) + " student(s) with ID " + studentId
+              + " deleted from course " + courseName);
         } else {
-          System.out.println(MenuConstants.WRONG_COURSE);
+          log.warn(MenuConstants.WRONG_COURSE);
         }
       } else {
-        System.out.println(MenuConstants.STUDENT_ID_NOT_EXIST);
+        log.warn(MenuConstants.STUDENT_ID_NOT_EXIST);
       }
     } else {
-      System.out.println(MenuConstants.DIGITS_REQUIRED);
+      log.warn(MenuConstants.DIGITS_REQUIRED);
     }
   }
 
   public void updateStudentByIdFacade(Scanner scan) {
-    System.out.println(MenuConstants.STUDENT_ID);
+    log.info(MenuConstants.STUDENT_ID);
     Integer studentId = scan.nextInt();
 
     if (studentService.getStudentID().contains(studentId)) {
-      System.out.println(MenuConstants.STUDENT_NAME);
+      log.info(MenuConstants.STUDENT_NAME);
       String newFirstName = scan.next();
-      System.out.println(MenuConstants.STUDENT_LAST_NAME);
+      log.info(MenuConstants.STUDENT_LAST_NAME);
       String newLastName = scan.next();
 
       if (!newFirstName.isEmpty() || !newLastName.isEmpty()) {
-        System.out.println(MenuConstants.GROUP_ID);
+        log.info(MenuConstants.GROUP_ID);
 
         if (scan.hasNextInt()) {
           Integer newGroupId = scan.nextInt();
           updateStudent(studentId, newFirstName, newLastName, newGroupId);
         } else {
-          System.out.println(MenuConstants.GROUP_ID_NOTE2);
+          log.warn(MenuConstants.GROUP_ID_NOTE2);
         }
       } else {
-        System.out.println(MenuConstants.EMPTY_NOTE);
+        log.warn(MenuConstants.EMPTY_NOTE);
       }
     } else {
-      System.out.println(MenuConstants.STUDENT_ID_NOT_EXIST);
+      log.warn(MenuConstants.STUDENT_ID_NOT_EXIST);
     }
   }
 
@@ -151,16 +155,16 @@ public class StudentMenuComponents {
         newGroupId = null;
       }
       Student updatedStudent = new Student(newGroupId, newFirstName, newLastName);
-      System.out.println(studentService.updateStudentById(studentId, updatedStudent) + " student updated" + "\n"
+      log.info(studentService.updateStudentById(studentId, updatedStudent) + " student updated" + "\n"
           + MenuConstants.STUDENT_MENU);
     } else {
-      System.out.println(MenuConstants.GROUP_ID_NOTE);
+      log.warn(MenuConstants.GROUP_ID_NOTE);
     }
   }
 
   public void showAllStudentsFacade() {
     studentService.showAllStudents().forEach(System.out::println);
-    System.out.println(MenuConstants.STUDENT_MENU);
+    log.info(MenuConstants.STUDENT_MENU);
   }
 
 }
